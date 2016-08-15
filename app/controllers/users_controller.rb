739f37c130a0d8bf46ddbcdb1dfe91a5467058fa
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-15 11:01:59 -0500

    change out user controller to simpler version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:username, :password)
   end
 end
