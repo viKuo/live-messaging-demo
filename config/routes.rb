acccfcbb464a09b1dff099b9679791202baa1c84
viKuo <lost.angel.fin@gmail.com>
2016-08-15 12:19:03 -0500

    non-live messaging works

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   	resources :chats, only:[:index, :create, :show]
   end 
   resources :sessions, only:[:new, :destroy, :create]
-
+	resources :messages, only:[:create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
