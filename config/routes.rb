26b0429b4f37503c5ba9cc1eaeb088f948aa8540
viKuo <lost.angel.fin@gmail.com>
2016-08-15 11:25:09 -0500

    add chats routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
 	root 'sessions#new'
 
-  resources :users, only:[:new, :create] 
+  resources :users, only:[:new, :create] do
+  	resources :chats, only:[:index, :craete, :show]
+  end 
   resources :sessions, only:[:new, :destroy, :create]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
