9200b7a717a65bdacf8db6c3fc3aedf03f17412e
viKuo <lost.angel.fin@gmail.com>
2016-08-15 12:03:27 -0500

    add messaging controller and views

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,24 @@
+class MessagesController < ApplicationController
+
+  def create
+    message = Message.new(message_params)
+    message.user = current_user
+
+    if message.save
+      ActionCable.server.broadcast( "messages_#{message_params[:chat_id]}",
+      #broadcasting out to messages channel, which all chats are linked to messages channel
+      message: message.content,
+      user: image,
+      username: message.user.first_name
+      )
+    else
+      redirect_to chats_path
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:content, :chat_id)
+    end
+end
+
