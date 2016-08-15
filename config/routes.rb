679be11124e17a1ce24d0a473adb22edc8d99c83
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-15 11:09:35 -0500

    change routes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 	root 'sessions#new'
-	
-  resources :users, except:[:show, :edit, :update] 
+
+  resources :users, only:[:new, :create] 
   resources :sessions, only:[:new, :destroy, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
