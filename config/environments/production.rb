d3a41eadd79b5effcf067cce75d3ce57d1724fe2
viKuo <lost.angel.fin@gmail.com>
2016-08-18 09:52:47 -0500

    config redis for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.web_socket_server_url = "wss://live-messaging-demo.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://live-messaging-demo.herokuapp.com', 'http://live-messaging-demo.herokuapp.com']
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
